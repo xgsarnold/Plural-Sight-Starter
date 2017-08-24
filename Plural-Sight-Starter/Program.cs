d3df01e1749066f4b283b517ce7c92321bede4b8
xgsarnold <geoffrey.s.arnold@gmail.com>
2017-08-24 16:25:03 -0500

    Added write line method to Main method

---
+++ Plural-Sight-Starter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plural_Sight_Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.DayOfWeek);
+        }
+    }
+}
